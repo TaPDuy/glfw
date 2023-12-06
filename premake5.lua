0e66426e3cce3ccd5e7918a19a9e0d3b11acaa05
Ta Phuong Duy <linhvu@trulet.com>
2023-12-06 11:53:43 +0700

    Update premake script

--- premake5.lua
+++ premake5.lua
@@ -8,11 +8,8 @@ project 'GLFW'
 	files {
 		'include/GLFW/glfw3.h',
 		'include/GLFW/glfw3native.h',
-		'src/context.c',
-		'src/init.c',
-		'src/input.c',
-		'src/monitor.c',
-		'src/window.c',
+		'src/**.h',
+		'src/**.c',
 	}
 
 	filter 'system:windows'
@@ -29,6 +26,12 @@ project 'GLFW'
 			'src/wgl_context.c',
 			'src/egl_context.c',
 			'src/osmesa_context.c'
+		}
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
 		}
 
 	filter 'configurations:Debug'
